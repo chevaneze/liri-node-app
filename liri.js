1ee8552017d10f43f1d728015a8983654ce15c96
chevaneze <cpsanon@gmail.com>
2019-02-21 19:26:08 -0500

    .

--- liri.js
+++ liri.js
@@ -1,37 +1,38 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var axios = require("axios");
-var moment = require("moment"); 
-var timeFormat = moment().format("ddd, MM/DD/YYYY");
+ require("dotenv").config();
+ var keys = require('./keys');
+ var axios = require("axios");
+ var moment = require("moment"); 
+ var Spotify = require("node-spotify-api")
+ let spotify = new Spotify(keys.spotify);
+ var timeFormat = moment().format("ddd, MM/DD/YYYY");
+ let command = process.argv[2];
+ let userSong = process.argv[3];
 
-let command = process.argv[2];
-let userSong = process.argv[3];
+ function spotifySearch () {
 
-function spotifySearch {
-let spotify = new Spotify(keys.spotify);
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-console.log("Artist: " + response )
-console.log("Track: " +response  ); 
-console.log("Preview URL: " + response);
-console.log("Album: "+response );
-});
+ spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, res) {
+   if (err) {
+     return console.log('Error occurred: ' + err);
+   }
+ console.log("artist " + res )
+ console.log("Track: " + res ); 
+ console.log("Preview URL: " + res);
+ console.log("Album: "+ res);
+ });
+}
+ spotifySearch();
 
-function movieSearch () {
-axios.get("http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy").then(
- function (response) {
+ function movieSearch () {
+ axios.get("http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy").then(
+  function (response) {
 
-console.log("Title: " + response);
-console.log("Release Year: " + response);
-console.log("IMDB Rating: " + response);
-console.log("Country Produced: " + response);
-console.log("Language: " + response);
+ console.log("Title: " + response);
+ console.log("Release Year: " + response);
+ console.log("IMDB Rating: " + response);
+ console.log("Country Produced: " + response);
+ console.log("Language: " + response);
 console.log("Plot: " + response);
 console.log("Actors: " + response );
-
       } };
  function concertSearch () {
 axios.get("https://rest.bandsintown.com/artists/" + userConcert + "/events?app_id=codingbootcamp").then(
